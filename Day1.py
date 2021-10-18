acdb393d29e04199c2a82dc3dbde4f5eac0937ec
LucianoWerneck <lulu_werneck@gmail.com.br>
2021-10-18 09:47:25 -0300

    Band Name Generator

--- Day1.py
+++ Day1.py
@@ -1,3 +1,4 @@
+#Program to generate band names
 print("Welcome to the Band Name Generator")
 city = input(str("What's name of the city you grew up in ?" ))
 pet = input(str("What's your pet's name? "))
