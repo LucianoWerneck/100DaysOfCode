746b172fbd646a8831387fac0f0f5aa98e03bc61
LucianoWerneck <lulu_werneck@gmail.com.br>
2021-10-18 10:19:54 -0300

    Beginner - Understanding Data Types and How to Manipulate Stringsa

--- Day2.py
+++ Day2.py
@@ -1,1 +1,7 @@
-#Program what we will make by the end of the day
+# tip calculator
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill?\n$"))
+tip = int(input("What percentage tip would you like to give(10, 12 or 15)?\n"))
+people = int(input("How many people to split the bill?\n"))
+total_amount_division = ((bill * tip/ 100) + bill) / people
+print(f"Each person should pay:\n${total_amount_division:.2f}ea")
