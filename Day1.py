dbcadd05b1d5af829d442477b987e7cf5d8a1037
LucianoWerneck <lulu_werneck@gmail.com.br>
2021-10-18 10:23:53 -0300

    Working with Variables in Python to Manage Data

--- Day1.py
+++ Day1.py
@@ -1,4 +1,4 @@
-#Program to generate band names
+#Working with Variables in Python to Manage Data
 print("Welcome to the Band Name Generator")
 city = input(str("What's name of the city you grew up in ?" ))
 pet = input(str("What's your pet's name? "))
