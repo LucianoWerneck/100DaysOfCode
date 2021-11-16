d08b7ebbcb393405b5342c0bdfe8a89d5924ce01
LucianoWerneck <lulu_werneck@gmail.com.br>
2021-11-16 15:51:13 -0300

    Function Parameters & Caesar Cipher

---
+++ Day8.py
@@ -0,0 +1,26 @@
+from art import logo
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar (direction_choice, start_text, shift_amount):
+    end_text = ""
+    if direction_choice == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {direction_choice} result: {end_text}")
+    question = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+
+question = "yes"
+while question != "no":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    caesar(direction_choice=direction, start_text=text, shift_amount=shift)
