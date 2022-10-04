870741e3a67219aa05e4dc252166f26f145abc06
LucianoWerneck <luciano.bartole@gmail.com>
2022-10-04 15:54:51 -0300

    teste Day_18

---
+++ Day_18/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("brown")
+tim.speed(2)
+
+for item in range(4):
+    tim.forward(100)
+    tim.right(90)
+
+
+
+import heroes
