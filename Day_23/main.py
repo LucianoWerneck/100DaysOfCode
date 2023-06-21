d5aed14960b10cddd468f58b007161ecd097a0e2
LucianoWerneck <luciano.bartole@gmail.com>
2023-06-20 21:11:28 -0300

    The Turtle Crossing Capstone Project

--- Day_23/main.py
+++ Day_23/main.py
@@ -9,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 # Command Key
 screen.listen()
@@ -18,5 +19,8 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_cars()
 
 screen.exitonclick()
