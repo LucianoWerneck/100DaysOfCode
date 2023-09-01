64e60a9b1f627d418d33ba12aa34e11de8626499
LucianoWerneck <luciano.bartole@gmail.com>
2023-09-01 11:01:27 -0300

    Files, Directories and Paths

--- Day_24/food.py
+++ Day_24/food.py
@@ -4,16 +4,20 @@ import random
 
 class Food(Turtle):
 
+    def color_food_random(self):
+        colors = ["red", "blue", "green", "cyan", "yellow", "pink", "purple", "gold", "magenta"]
+        self.color(random.choice(colors))
+
     def __init__(self):
         super().__init__()
-        self.shape("circle")
+        self.shape("turtle")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color("blue")
-        self.speed("fastest")
+        self.shapesize(stretch_len=0.9, stretch_wid=0.9)
+        self.speed("fast")
         self.refresh()
 
     def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
+        self.color_food_random()
         self.goto(random_x, random_y)
